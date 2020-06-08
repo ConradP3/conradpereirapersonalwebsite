ecf13a3e9c0a4af9088c2cd73ffa393c94cf055d
colperei <colperei@ucsc.edu>
2020-06-07 21:30:26 -0700

    Live

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,8 +7,8 @@ class Resume extends Component {
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p className="info">{education.degree}  <em className="date">{education.graduated}</em></p>
+         <p>{education.description}</p></div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
@@ -18,7 +18,7 @@ class Resume extends Component {
       })
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+        return <li key={skills.name}><em>{skills.name}</em></li>
       })
     }
 
