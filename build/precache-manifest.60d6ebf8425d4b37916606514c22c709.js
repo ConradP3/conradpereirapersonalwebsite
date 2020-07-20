18a299b6fb4a25016fa7538abf117fa91be96322
colperei <colperei@ucsc.edu>
2020-07-20 03:34:21 -0700

    update

---
+++ build/precache-manifest.60d6ebf8425d4b37916606514c22c709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f1bbfc2c21ec75b6ce6abbe9431531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38e221dc06455c24ec6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3f3785950fb1fea3b3ca",
+    "url": "/static/js/2.bbcea1cb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.bbcea1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38e221dc06455c24ec6",
+    "url": "/static/js/main.30ce8a3c.chunk.js"
+  },
+  {
+    "revision": "2bb5066a0e2bd0fb47d7",
+    "url": "/static/js/runtime-main.2455f2d1.js"
+  }
+]);
